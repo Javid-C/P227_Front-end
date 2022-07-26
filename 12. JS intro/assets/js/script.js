ceb10084e8c881c25bef53685f5beffaf2488b6d
Javid-C <javidia@code.edu.az>
2022-07-26 18:12:09 +0400

    JS intro

---
+++ 12. JS intro/assets/js/script.js
@@ -0,0 +1,123 @@
+// var data = prompt("Please enter your age");
+
+// confirm("Are you sure about that");
+// console.log(data);
+
+// var fullname = "Xeyal Mammadzade";
+
+// var fullname = "Ali";
+
+// let fname = "Gunel";
+
+// fname = "Ali";
+
+// console.log(fname);
+
+// var number2 = number + 2;
+// console.log(number2);
+
+// var number = 5;
+
+// let age = 25;
+
+// const access = false;
+
+// console.log(age);
+// console.log(access);
+
+// const number = 5;
+
+// const numbers = 10;
+
+// console.log(number);
+
+let bool = true;
+
+let nullable = null;
+
+let undefinedd = undefined;
+
+let number = 5;
+
+let bignint = 230n;
+
+let string = "okay";
+
+let number2 = "5";
+// console.log(number == number2);
+// console.log(typeof typeof bool);
+// console.log(typeof typeof nullable);
+// console.log(typeof typeof undefinedd);
+// console.log(typeof typeof number);
+// console.log(typeof typeof bignint);
+// console.log(typeof typeof string);
+
+// let array = [1, 2, 3, "Ali", { name: "Rafet" }, null, undefined, 54n];
+
+// console.log(array[1000]);
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// for (let key of array) {
+//   console.log(key);
+// }
+
+let index = 0;
+
+// while (index <= 20) {
+//   console.log(index);
+//   index++;
+// }
+
+// for (let i = 0; i < 150; i++) {
+//   if (i == 100) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// console.log(index++);
+// console.log(index);
+
+// let day = 3;
+
+// switch (day) {
+//   case (1, 2, 3):
+//     console.log("Monday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+
+//   default:
+//     console.log("Please choose valid day");
+//     break;
+// }
+
+// try {
+//   const number = 5;
+//   number = 6;
+// } catch (error) {
+//   throw new Error(error);
+// } finally {
+//   console.log("after catch");
+// }
+
+// console.log("after finally");
+
+// if (NaN) {
+//   console.log("test");
+// }
+
+// var test = 0 || "" || "okay" || "" || "test";
+// console.log(test);
