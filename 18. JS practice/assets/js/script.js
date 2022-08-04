20cfa3bc30ff77303bddb21895807daf281a342e
Javid-C <javidia@code.edu.az>
2022-08-04 17:26:44 +0400

    Bubble and capturing

---
+++ 18. JS practice/assets/js/script.js
@@ -0,0 +1,50 @@
+// let tabs = document.querySelectorAll(".tab");
+// tabs.forEach((tab) => {
+//   tab.onclick = function () {
+//     let id = tab.getAttribute("data-id");
+//     let content = document.getElementById(id);
+//     content.classList.add("active");
+//     let active = document.querySelector(".active");
+//     if (active) {
+//       active.classList.remove("active");
+//     }
+//   };
+// });
+
+let btn = document.querySelector(".btn");
+let container = document.querySelector(".container");
+let row = document.querySelector(".row");
+
+// btn.onclick = function () {
+//   console.log("first process");
+// };
+
+// btn.addEventListener("click", function () {
+//   console.log("first process");
+// });
+
+// btn.addEventListener("click", function () {
+//   console.log("second process");
+// });
+
+// btn.addEventListener("dblclick", function (e) {
+//   console.log(e.target);
+// });
+
+// container.addEventListener("click", function (event) {
+//   //   console.log(this);
+// //   console.log(event.target);
+// });
+
+// btn.addEventListener("click", function (e) {
+//   e.stopPropagation();
+//   alert(e.target);
+// });
+
+// container.addEventListener("click", function (e) {
+//   alert(e.target);
+// });
+
+// row.addEventListener("click", function (e) {
+//   alert(e.target);
+// });
